ea842b3c32a238f81be050bc3429e3afbe53c7af
AleMCuitino <ale.morales11@gmail.com>
2023-02-23 20:18:03 +0100

    Added changes in body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import ButtonAdd from './ButtonAdd';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
-function Body() {
-  
-  const [products, setProducts] = useState([]);
+// import { Link } from 'react-router-dom';
 
-  useEffect(() => {
-    fetch('url_del_json')
-      .then(response => response.json())
-      .then(data => setProducts(data.products))
-      .catch(error => console.error(error))
-  }, [])
+const url = 'http://localhost:8000/api'
 
-  return (
-    <div>
-      {products.map(product => (
-        <div key={product.id}>
-          <img src= "https://m.media-amazon.com/images/I/61B0IouivwL._SY355_.jpg" alt="" />
-          <h3>{product.bandName}</h3>
-          <h3>{product.albumName}</h3>
-          <p>{product.price}</p>
-          <ButtonAdd />
+const Body = () => {
+
+    const [vinils, setVinils] = useState([]);
+
+    useEffect(() => {
+        getAllElements();
+    }, [])
+
+    const getAllElements = async () => {
+        const response = await axios.get(`${url}/vinils`);
+        setVinils(response.data);
+    }
+
+    return (
+        <div>
+            <div>
+                <h2> Listado de vinilos </h2>
+                {
+                    vinils.map((vinil) => (
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg" key={vinil.id}>
+                            <img className="w-full" src={vinil.image} alt="Sunset in the mountains" />
+                            <div className="px-6 py-4">
+                                <div class="font-bold text-xl mb-2">{vinil.bandName}</div>
+                                <p class="text-gray-700 text-base"> {vinil.albumName}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default Body;
